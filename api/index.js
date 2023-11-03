1c6578e81398d5d89cdb7235e8b8ea040270caed
yash shrestha <yashcrest@gmail.com>
2023-11-03 12:52:04 +1030

    savings changes

--- api/index.js
+++ api/index.js
@@ -1,42 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const Stripe = require("stripe");
 
 const app = express();
 
-// use this to allow all origins.
-app.use(cors());
-
+// Use this to allow specific origin.
 app.use(
   cors({
     origin: "https://ecommerce.yashshrestha.net",
   })
 );
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
-const Stripe = require("stripe");
 const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ||
     "sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1"
 );
 
-const handler = async (req, res) => {
+app.post("/api/checkout", async (req, res) => {
   try {
     const items = req.body.products;
     let lineItems = items.map((item) => ({
@@ -53,9 +36,14 @@ const handler = async (req, res) => {
 
     res.json({ url: session.url });
   } catch (error) {
-    console.error("Error creating string session: ", error);
+    console.error("Error creating stripe session: ", error);
     res.status(500).json({ error: "Error creating stripe session" });
   }
-};
+});
 
-module.exports = allowCors(handler);
+// Here you can add additional endpoints if required
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
