d84ccec5292e7612f3dc55cf6c8ae58648973de9
yash shrestha <yashcrest@gmail.com>
2023-11-05 20:40:34 +1030

    enabling cors

--- api/index.js
+++ api/index.js
@@ -5,20 +5,25 @@ const cors = require("cors");
 const app = express();
 
 //cors config testing
-const options = [
-  cors({
-    origin: "https://ecommerce.yashshrestha.net",
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-];
+// const options = [
+//   cors({
+//     origin: "https://ecommerce.yashshrestha.net",
+//     methods: "GET,POST,PUT,DELETE,OPTIONS",
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   }),
+// ];
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
-app.use(options);
+// app.use(options);
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   console.log(`${req.method} ${req.path}`);
   next();
 });
@@ -27,6 +32,7 @@ const stripe = new Stripe(
   "sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1"
 );
 
+//Creating stripe checkout session
 app.post("/checkout", async (req, res) => {
   try {
     const items = req.body.products;
