088b6fcab08442634274e6570a7cde0f945e4222
yash shrestha <yashcrest@gmail.com>
2023-11-06 10:28:47 +1030

    removing body parser

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Stripe = require("stripe");
 const microCors = require("micro-cors");
 // const cors = require("cors");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 const app = express();
 const cors = microCors();
