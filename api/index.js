a37b23d64261c9bef200668e56de86325ca952a5
Yash Shrestha <79971012+yashcrest@users.noreply.github.com>
2023-11-06 13:37:26 +1030

    removed micro-cors

--- api/index.js
+++ api/index.js
@@ -1,29 +1,51 @@
 const express = require("express");
 const Stripe = require("stripe");
 const microCors = require("micro-cors");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-const cors = microCors();
+// const cors = microCors();
 
 const stripe = new Stripe(
   "sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1"
 );
 
 //micro-cors middleware
-app.use((req, res, next) => {
-  cors(req, res);
+// app.use((req, res, next) => {
+//   cors(req, res);
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).send("ok");
-  }
+//   if (req.method === "OPTIONS") {
+//     return res.status(200).send("ok");
+//   }
 
-  next();
-});
+//   next();
+// });
+
+
+// CORS middleware setup
+// Configure CORS to allow requests from your front-end domain
+const corsOptions = {
+  origin: "https://react-e-commerce-kappa.vercel.app"
+, // Your front-end origin
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Content-Type",
+    "Date",
+    "X-Api-Version"
+  ],
+};
+
 
 //using cors
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 //Creating stripe checkout session
