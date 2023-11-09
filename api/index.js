9d8e7239d36b4e2ec0d022147646c47b7d692bdf
yashcrest <yashcrest@gmail.com>
2023-11-09 12:58:48 +1030

    adding vercel recommend middleware

--- api/index.js
+++ api/index.js
@@ -1,31 +1,38 @@
 const express = require("express");
 const Stripe = require("stripe");
 const bodyParser = require("body-parser");
-const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
 const stripe = new Stripe(
   "sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1"
 );
 
+// CORS middleware wrapper
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://react-e-commerce-kappa.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-//Preflight request handler
-const handePreflightRequest = (req, res, next) => {
-    if(req.method === 'OPTIONS') {
-        res.status(200).send('ok');
-    } else {
-        next();
-    }
-}
-
-//use preflight request handler as application-levle middleware
-app.use(handePreflightRequest);
-
-//Creating stripe checkout session
-app.post("/checkout", async (req, res) => {
+// Stripe checkout session function
+const createCheckoutSession = async (req, res) => {
   try {
     const items = req.body.products;
     let lineItems = items.map((item) => ({
@@ -44,11 +51,10 @@ app.post("/checkout", async (req, res) => {
   } catch (error) {
     console.error("Error creating stripe session: ", error);
     res.status(500).json({ error: "Error creating stripe session" });
-  }  
-});
+  }
+};
 
+//Creating stripe checkout session
+app.post("/checkout", allowCors(createCheckoutSession));
 
-// const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
