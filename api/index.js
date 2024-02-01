78beaaae022cc9a5c7c49638488a50aa121bb8fa
Yash Shrestha <79971012+yashcrest@users.noreply.github.com>
2024-02-01 13:48:41 +1030

    using cors package

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const app = express();
 require("dotenv").config();
 const stripe = require("stripe")('sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1');
 const bodyParser = require("body-parser");
+
+
+//using cors package
+const cors = require('cors');
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,24 +15,11 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 4000;
 
 // CORS middleware wrapper
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://react-e-commerce-kappa.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(204).end();
-    return;
-  }
-  return await fn(req, res);
+const corsOptions = {
+  origin: 'https://ecommerce.yashshrestha.net',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
 };
 
 // Stripe checkout session function
@@ -56,6 +47,8 @@ const createCheckoutSession = async (req, res) => {
 
 //Creating stripe checkout session
 app.post("/checkout", allowCors(createCheckoutSession));
-// app.listen(port, () => console.log(`Server running port ${port}`));
+
+//enabling CORS for routes
+app.use(cors(corsOptions));
 
 module.exports = app;
