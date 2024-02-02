da148a7ced6997a8b1de077514841b04658bfa8f
Yash Shrestha <79971012+yashcrest@users.noreply.github.com>
2024-02-02 11:24:18 +1100

    restructuring for azure app service

--- api/index.js
+++ api/index.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const stripe = require("stripe")('sk_test_51NxIMbIIas9tFQMRc0T9EYd6DS8Isn1XF5BctEHFqU9eSS7DtFmm9yt2wOtGdFmyqkYuRvrRRo6zcPOVpgKA7sKG009t3rbFH1');
-const bodyParser = require("body-parser");
 
-
-//using cors package
-const cors = require('cors');
-app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const port = process.env.PORT || 4000;
+const app = express();
 
 // CORS middleware wrapper
 const corsOptions = {
@@ -22,6 +14,14 @@ const corsOptions = {
   optionsSuccessStatus: 204
 };
 
+//Middleware
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
 // Stripe checkout session function
 const createCheckoutSession = async (req, res) => {
   try {
@@ -30,14 +30,14 @@ const createCheckoutSession = async (req, res) => {
       price: item.id,
       quantity: item.quantity,
     }));
-
+    
     const session = await stripe.checkout.sessions.create({
       line_items: lineItems,
       mode: "payment",
       success_url: 'https://ecommerce.yashshrestha.net/success',
       cancel_url: 'https://ecommerce.yashshrestha.net/failed',
     });
-
+    
     res.send(JSON.stringify({ url: session.url }));
   } catch (error) {
     console.error("Error creating stripe session: ", error);
@@ -48,7 +48,6 @@ const createCheckoutSession = async (req, res) => {
 //Creating stripe checkout session
 app.post("/checkout", allowCors(createCheckoutSession));
 
-//enabling CORS for routes
-app.use(cors(corsOptions));
 
-module.exports = app;
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
